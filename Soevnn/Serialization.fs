edc407fd30d8658e72016426f114455e405ea49a
Lilith Stanley <lilith.tsukie@gmail.com>
2021-08-03 10:24:30 -0700

    Documents Serialization.fs.

--- Soevnn/Serialization.fs
+++ Soevnn/Serialization.fs
@@ -5,6 +5,10 @@ open MessagePack.FSharp
 open MessagePack.Resolvers
 open MessagePack.Formatters
 
+// Pretty sure this doesn't properly deserialize multiple references to the same value.
+// This doesn't currently matter as any such cases are instead referenced in a system agnostic manner via mechanisms such as NeuralAddress.
+// Should probably fix this at some point anyways.
+///  <summary> Is a formatter for the Ref type. Provides methods for serializing and deserializing the Ref type. </summary>
 type RefFormatter<'T>() =
     interface IMessagePackFormatter<Ref<'T>> with
         member this.Deserialize(reader: byref<MessagePackReader>, options: MessagePackSerializerOptions): Ref<'T> = 
@@ -16,19 +20,19 @@ type RefFormatter<'T>() =
         member this.Serialize(writer: byref<MessagePackWriter>, value: Ref<'T>, options: MessagePackSerializerOptions): unit = 
             options.Resolver.GetFormatterWithVerify<'T>().Serialize(&writer, value.Value, options)
         
-
+/// <summary> A cache of formatters to be used for serialization/deserialization. </summary>
 type private Cache<'T> =
     static member Formatter : IMessagePackFormatter<'T> =
         
-        if typedefof<'T> = typedefof<Ref<int>> then
+        if typedefof<'T> = typedefof<Ref<int>> then // Handle the special case of reference cells when applicable.
             let td = typeof<'T>
-            let subt = td.GenericTypeArguments.[0]
+            let subt = td.GenericTypeArguments.[0] // Get the type parameter.
             let refformtd = typedefof<RefFormatter<int>>
-            let reformt = refformtd.MakeGenericType([|subt|])
+            let reformt = refformtd.MakeGenericType([|subt|]) // Create a RefFormatter with the same type parameter.
             //let reformc = reformt.GetConstructor(System.Type.EmptyTypes)
             
-            let rf = System.Activator.CreateInstance(reformt) // reformc.Invoke([||])
-            rf :?> IMessagePackFormatter<'T>
+            let rf = System.Activator.CreateInstance(reformt) // reformc.Invoke([||]) // Instantiate the RefFormatter.
+            rf :?> IMessagePackFormatter<'T> // Cast to the return type.
         else
             let r =
                 Array.tryFind
@@ -40,9 +44,10 @@ type private Cache<'T> =
             | Some r -> r.GetFormatter<'T>()
             | _ -> null
 
+/// <summary> The resolver to be used for Soevnn. </summary>
 and SoevnnResolver() =
-    static member Instance = new SoevnnResolver()
-    static member Resolvers : IFormatterResolver[] = 
+    static member Instance = new SoevnnResolver() // Provides the instance of the resolver.
+    static member Resolvers : IFormatterResolver[] = // Provides the instances of the base resolvers.
         [|
             FSharpResolver.Instance;
             StandardResolver.Instance;
@@ -56,16 +61,19 @@ and SoevnnResolver() =
     
 
 
-
+// The instanced resolver.
 let Resolver =
     SoevnnResolver.Instance
 
+// The options for serialization/deserialization. 
 let MessagePackOptions = 
     MessagePackSerializerOptions.Standard.WithResolver(Resolver).WithAllowAssemblyVersionMismatch(true)
 
+/// <summary> Saves a nervous system. </summary>
 let SaveNervousSystem (ns : NervousSystem) (na : (NeuralAddress * Neuron) array) (stream : System.IO.Stream) =
     MessagePackSerializer.Serialize(stream,(ns,na),MessagePackOptions)
      
+/// <summary> Tries to load a nervous system. </summary>
 let LoadNervousSystem (stream : System.IO.Stream) =
     try
         Some <| MessagePackSerializer.Deserialize<NervousSystem * (NeuralAddress * Neuron) array>(stream,MessagePackOptions)
